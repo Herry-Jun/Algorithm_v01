e0aa41021a7bae71ebebef15804f99a1000cda09
Herry-Jun <wnsrlrla0@gmail.com>
2024-12-27 13:22:20 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    
+    int answer = 0;
+    
+    if( n % 2 == 0){
+        answer = n;
+    }
+    else{
+        answer = 0;
+    }
+    for(int i = 1; i < n; i++ ){
+        
+        if(i % 2 == 0){
+            
+            answer += i;
+        }
+        
+    }
+    
+    
+    return answer;
+}
