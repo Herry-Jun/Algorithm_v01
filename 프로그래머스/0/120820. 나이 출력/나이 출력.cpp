752708678d290d1994f10aec085debdbb8ce6038
Herry-Jun <wnsrlrla0@gmail.com>
2024-12-27 13:21:05 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) {
+    
+    int result = 2022;
+    int answer =  result - age + 1;
+    
+    return answer;
+}
